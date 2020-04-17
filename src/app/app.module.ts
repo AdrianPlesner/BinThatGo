100e284a8d8f199bbbfb3916791cd72f8d5f6e85
Adrian Plesner <adrian.plesner@gmail.com>
2020-04-17 14:53:38 +0200

    Create game form and current games list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListOfGamesComponent } from './list-of-games/list-of-games.component';
+import {RouterModule} from '@angular/router';
+import { CreateGameComponent } from './create-game/create-game.component';
+import { BingoGameComponent } from './bingo-game/bingo-game.component';
+import {ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: '', component: ListOfGamesComponent},
+      {path: 'create', component: CreateGameComponent},
+      {path: 'bingo/:gameId', component: BingoGameComponent}
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    ListOfGamesComponent,
+    CreateGameComponent,
+    BingoGameComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
